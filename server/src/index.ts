6045caa0f3304c3d4a1ae94d0f76c34ca8e53d80
Sanjay Rajmane <63235956+sanjay-rajmane@users.noreply.github.com>
2025-05-09 16:52:31 +0530

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,57 +1,47 @@
-import express, { Request, Response } from "express"
-import { Server } from "@modelcontextprotocol/sdk/server/index.js"
-import { MCPServer } from "./server.js"
+import express from "express";
+import { Server } from "@modelcontextprotocol/sdk/server/index.js";
+import { MCPServer } from "./server.js";
+import cors from "cors";
 
-/*******************************/
-/******* Server Set Up *******/
-/*******************************/
+const PORT = 3000;
 
-const server = new MCPServer(
-    new Server({
-        name: "remote-mcp-server",
-        version: "1.0.0"
-    }, {
-        capabilities: {
-            tools: {},
-            logging: {}
-        }
-    })
-)
-
-/*******************************/
-/******* Endpoint Set Up *******/
-/*******************************/
+const server = new MCPServer(new Server({
+    name: "remote-mcp-server",
+    version: "1.0.0"
+}, {
+    capabilities: {
+        tools: {},
+        logging: {}
+    }
+}));
 
 // to support multiple simultaneous connections
+const app = express();
+const router = express.Router();
 
-const app = express()
-app.use(express.json())
-
-const router = express.Router()
+app.use(express.json());
+app.use(cors());
 
 // endpoint for the client to use for sending messages
-const MCP_ENDPOINT = "/mcp"
-
-// handler
-router.post(MCP_ENDPOINT, async (req: Request, res: Response) => {
-    await server.handlePostRequest(req, res)
-})
+const MCP_ENDPOINT = "/mcp";
+router.post(MCP_ENDPOINT, async (req: any, res: any) => {
+    await server.handlePostRequest(req, res);
+});
 
 // Handle GET requests for SSE streams (using built-in support from StreamableHTTP)
-router.get(MCP_ENDPOINT, async (req: Request, res: Response) => {
-    await server.handleGetRequest(req, res)
-})
+router.get(MCP_ENDPOINT, async (req: any, res: any) => {
+    await server.handleGetRequest(req, res);
+});
 
 
-app.use('/', router)
+app.use('/', router);
 
-const PORT = 3000
 app.listen(PORT, () => {
-    console.log(`MCP Streamable HTTP Server listening on port ${PORT}`)
-})
+    console.log(`MCP Streamable HTTP Server listening on port ${PORT}`);
+});
 
 process.on('SIGINT', async () => {
-    console.log('Shutting down server...')
-    await server.cleanup()
-    process.exit(0)
-})
+    console.log('Shutting down server...');
+    await server.cleanup();
+    process.exit(0);
+});
